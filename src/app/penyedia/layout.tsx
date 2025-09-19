514967ffe84a744c829abb18fc24c8335fce920e
Mochamad Abdurrochman Azis <Axzis11@gmail.com>
2025-09-19 06:19:29 +0000

    /dashboard/penyedia/tawaran masih belum muncul

---
+++ src/app/penyedia/layout.tsx
@@ -0,0 +1,17 @@
+
+import { Footer } from "@/components/layout/footer";
+import { Navbar } from "@/components/layout/navbar";
+
+export default function ProviderPublicLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Navbar />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+}
